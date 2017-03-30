26cefdbc12450a297adb19b64fef448f201f73a2
AnglePan <136761657@qq.com>
2017-03-30 17:15:06 +0800

    add logo

--- packages/rocketchat-livechat/client/views/app/livechatInstallation.js
+++ packages/rocketchat-livechat/client/views/app/livechatInstallation.js
@@ -2,7 +2,7 @@ Template.livechatInstallation.helpers({
 	script() {
 		const siteUrl = s.rtrim(RocketChat.settings.get('Site_Url'), '/');
 
-		return `<!-- Start of Rocket.Chat Livechat Script -->
+		return `<!-- Start of Livechat Script -->
 <script type="text/javascript">
 (function(w, d, s, u) {
 	w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
@@ -11,6 +11,6 @@ Template.livechatInstallation.helpers({
 	h.parentNode.insertBefore(j, h);
 })(window, document, 'script', '${siteUrl}/livechat');
 </script>
-<!-- End of Rocket.Chat Livechat Script -->`;
+<!-- End of Livechat Script -->`;
 	}
 });
